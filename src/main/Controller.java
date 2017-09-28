31501e31332a99802d4d416b022cebc67aa7a9ca
chris3x33 <chris3x33g@gmail.com>
2017-09-27 19:32:27 -0500

    Initial commit

---
+++ src/main/Controller.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.stage.Stage;
+
+public class Controller {
+
+    public static final int WINDOWHEIGHT = Main.WINDOWHEIGHT;
+    public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
+    public static final String APP_TITLE = Main.APP_TITLE;
+    public static final Stage PRIMARY_STAGE = Main.primaryStage;
+
+
+
+    public void initialize() {
+
+    }
+
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static final String APP_TITLE = "Sample";
+    public static final int WINDOWHEIGHT = 275;
+    public static final int WINDOWWIDTH = 400;
+    public static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setScene(new Scene(root, WINDOWWIDTH, WINDOWHEIGHT));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
