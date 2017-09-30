76e48ab13b8e4c8c193ec948722747312e79169e
chris3x33 <chris3x33g@gmail.com>
2017-09-30 09:37:30 -0500

    Created NewUserController

---
+++ src/controllers/NewUserController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import atmClient.ATMClient;
+import javafx.stage.Stage;
+import main.Main;
+
+public class NewUserController {
+
+    public static final int WINDOWHEIGHT = Main.WINDOWHEIGHT;
+    public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
+    public static final String APP_TITLE = Main.APP_TITLE;
+    public static final Stage PRIMARY_STAGE = Main.primaryStage;
+
+    public static ATMClient atmClient = Main.atmClient;
+
+
+    public void initialize() {
+
+    }
+
+
+}
