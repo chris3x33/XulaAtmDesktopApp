8644090737d6b47c8242f27d4536d791a04ee815
chris3x33 <chris3x33g@gmail.com>
2017-09-30 08:36:22 -0500

    Created LoginController

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import atmClient.ATMClient;
+import javafx.stage.Stage;
+import main.Main;
+
+public class LoginController {
+
+    public static final int WINDOWHEIGHT = Main.WINDOWHEIGHT;
+    public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
+    public static final String APP_TITLE = Main.APP_TITLE;
+    public static final Stage PRIMARY_STAGE = Main.primaryStage;
+    public static ATMClient atmClient = Main.atmClient;
+
+
+    public void initialize() {
+
+    }
+
+
+}
