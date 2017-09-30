56f68893e6ffa0d55aa6439bbd24888f67f5b4be
chris3x33 <chris3x33g@gmail.com>
2017-09-30 08:41:31 -0500

    Created Global loginMsgLbl:Label in LoginController

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import atmClient.ATMClient;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import main.Main;
 
@@ -10,7 +11,10 @@ public class LoginController {
     public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
     public static final String APP_TITLE = Main.APP_TITLE;
     public static final Stage PRIMARY_STAGE = Main.primaryStage;
+
     public static ATMClient atmClient = Main.atmClient;
+
+    public Label loginMsgLbl;
 
 
     public void initialize() {
