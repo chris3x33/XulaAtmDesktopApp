00522882c8125e5bb650a42d0968bf23a5d600e4
chris3x33 <chris3x33g@gmail.com>
2017-09-29 20:09:29 -0500

    Created Global atmClient:ATMClient in Controller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,6 @@
 package main;
 
+import atmClient.ATMClient;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -8,7 +9,7 @@ public class Controller {
     public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
     public static final String APP_TITLE = Main.APP_TITLE;
     public static final Stage PRIMARY_STAGE = Main.primaryStage;
-
+    public static ATMClient atmClient = Main.atmClient;
 
 
     public void initialize() {
