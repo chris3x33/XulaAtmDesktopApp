e34d9d73c1a027ed482a4e8a85c886abc4caa952
chris3x33 <chris3x33g@gmail.com>
2017-10-03 16:48:40 -0500

    Added GUI components in UserHomeScene and UserHomeController

--- src/controllers/UserHomeController.java
+++ src/controllers/UserHomeController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import atmClient.ATMClient;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import main.Main;
 
@@ -11,6 +13,7 @@ public class UserHomeController {
     public static final String APP_TITLE = Main.APP_TITLE;
     public static final Stage PRIMARY_STAGE = Main.primaryStage;
     public static ATMClient atmClient = Main.atmClient;
+    public Label userHomeMsgLbl;
 
 
     public void initialize() {
@@ -18,4 +21,15 @@ public class UserHomeController {
     }
 
 
+    public void runViewAccounts(ActionEvent actionEvent) {
+    }
+
+    public void runTransfer(ActionEvent actionEvent) {
+    }
+
+    public void runDeposit(ActionEvent actionEvent) {
+    }
+
+    public void runWithdraw(ActionEvent actionEvent) {
+    }
 }
