e4e10e24f0dcef4fd0c0b9e68f19a4e412062c3e
chris3x33 <chris3x33g@gmail.com>
2017-10-05 12:14:07 -0500

    Created Result

---
+++ src/atmClient/Result.java
@@ -0,0 +1,42 @@
+package atmClient;
+
+public class Result {
+
+    private int status;// 0 for fail, 1 for success
+    private String message;
+
+    public Result() {
+
+        // sets status to 1 for a success
+        this.status = 1;
+
+        this.message = "";
+
+    }
+
+    public Result(int status) {
+
+        // sets status
+        this.status = status;
+
+        this.message = "";
+
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
