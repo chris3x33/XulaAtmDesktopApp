c02522ee0beb561bce41bbfbd74cf54d014941f2
chris3x33 <chris3x33g@gmail.com>
2017-10-09 14:43:02 -0500

    Created readIntWTimeout(socket:Socket,dataIn:DataInputStream,timeOut:int):int in ATMClient

--- src/atmClient/ATMClient.java
+++ src/atmClient/ATMClient.java
@@ -1,5 +1,6 @@
 package atmClient;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.*;
 
@@ -69,6 +70,28 @@ public class ATMClient {
         return new Result(Result.SUCCESS_CODE);
 
     }
+
+
+
+    private int readIntWTimeout(Socket socket, DataInputStream dataIn, int timeOut) throws IOException {
+
+        final int BYTE_SIZE_OF_INT = Integer.SIZE/ Byte.SIZE;
+
+        boolean hasInt;
+
+        long startTime = System.currentTimeMillis();
+
+        do {
+            hasInt = (socket.getInputStream().available() >= BYTE_SIZE_OF_INT);
+        } while (!hasInt && (System.currentTimeMillis() - startTime) < timeOut);
+
+        if (hasInt) {
+            return dataIn.readInt();
+        } else {
+            throw new SocketTimeoutException();
+        }
+    }
+
 
     public int getTimeOut(){
         if (userDefinedTimeOut > -1){
