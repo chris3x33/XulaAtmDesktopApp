438b62553edc6ffa400c03eb4f5fc51812f06402
chris3x33 <chris3x33g@gmail.com>
2017-10-13 14:38:12 -0500

    Created Global sessionMessage:String in SessionResult

--- src/atmClient/SessionResult.java
+++ src/atmClient/SessionResult.java
@@ -3,7 +3,7 @@ package atmClient;
 public class SessionResult extends Result{
 
     private int sessionStatus;
-
+    private String sessionMessage;
     public SessionResult(int sessionStatus,int status) {
         super(status);
         this.sessionStatus = sessionStatus;
@@ -13,6 +13,19 @@ public class SessionResult extends Result{
     public SessionResult(int sessionStatus, int status, String message) {
         super(status, message);
         this.sessionStatus = sessionStatus;
+    }
+
+    public SessionResult(int sessionStatus,String sessionMessage, int status) {
+        super(status);
+        this.sessionStatus = sessionStatus;
+        this.sessionMessage = sessionMessage;
+
+    }
+
+    public SessionResult(int sessionStatus, String sessionMessage, int status, String message) {
+        super(status, message);
+        this.sessionStatus = sessionStatus;
+        this.sessionMessage = sessionMessage;
     }
 
 }
