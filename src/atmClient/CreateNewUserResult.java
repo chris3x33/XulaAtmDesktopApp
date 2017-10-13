4fa5c98bcdc6e52aca68e1b1a6cd27e767c0a31e
chris3x33 <chris3x33g@gmail.com>
2017-10-13 14:44:38 -0500

    Created CreateNewUserResult

---
+++ src/atmClient/CreateNewUserResult.java
@@ -0,0 +1,21 @@
+package atmClient;
+
+public class CreateNewUserResult extends SessionResult {
+
+    public CreateNewUserResult(int sessionStatus, int status) {
+        super(sessionStatus, status);
+    }
+
+    public CreateNewUserResult(int sessionStatus, int status, String message) {
+        super(sessionStatus, status, message);
+    }
+
+    public CreateNewUserResult(int sessionStatus, String sessionMessage, int status) {
+        super(sessionStatus, sessionMessage, status);
+    }
+
+    public CreateNewUserResult(int sessionStatus, String sessionMessage, int status, String message) {
+        super(sessionStatus, sessionMessage, status, message);
+    }
+
+}
