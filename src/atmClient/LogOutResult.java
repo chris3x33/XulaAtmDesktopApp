9c6d8b750f2a56a262d76f82286ffd870fe5f680
chris3x33 <chris3x33g@gmail.com>
2017-10-15 10:17:55 -0500

    Created LogOutResult

---
+++ src/atmClient/LogOutResult.java
@@ -0,0 +1,20 @@
+package atmClient;
+
+public class LogOutResult extends SessionResult {
+
+    public LogOutResult(int sessionStatus, int status) {
+        super(sessionStatus, status);
+    }
+
+    public LogOutResult(int sessionStatus, int status, String message) {
+        super(sessionStatus, status, message);
+    }
+
+    public LogOutResult(int sessionStatus, String sessionMessage, int status) {
+        super(sessionStatus, sessionMessage, status);
+    }
+
+    public LogOutResult(int sessionStatus, String sessionMessage, int status, String message) {
+        super(sessionStatus, sessionMessage, status, message);
+    }
+}
