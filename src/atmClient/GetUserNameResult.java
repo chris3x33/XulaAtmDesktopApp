b5cbfe6ff6281d63f513353f6693ebe69af1c3f6
chris3x33 <chris3x33g@gmail.com>
2017-10-19 07:23:08 -0500

    Created GetUserNameResult

---
+++ src/atmClient/GetUserNameResult.java
@@ -0,0 +1,22 @@
+package atmClient;
+
+public class GetUserNameResult extends SessionResult {
+
+    private String userName = "";
+
+    public GetUserNameResult(int sessionStatus, int status) {
+        super(sessionStatus, status);
+    }
+
+    public GetUserNameResult(int sessionStatus, int status, String message) {
+        super(sessionStatus, status, message);
+    }
+
+    public GetUserNameResult(int sessionStatus, String sessionMessage, int status) {
+        super(sessionStatus, sessionMessage, status);
+    }
+
+    public GetUserNameResult(int sessionStatus, String sessionMessage, int status, String message) {
+        super(sessionStatus, sessionMessage, status, message);
+    }
+}
