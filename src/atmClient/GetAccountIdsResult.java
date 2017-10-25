7501e291954a8f77f3c74cb1ab92f543e2c804cd
chris3x33 <chris3x33g@gmail.com>
2017-10-25 13:51:45 -0500

    Created GetAccountIdsResult

---
+++ src/atmClient/GetAccountIdsResult.java
@@ -0,0 +1,34 @@
+package atmClient;
+
+import java.util.ArrayList;
+
+public class GetAccountIdsResult extends SessionResult {
+
+    private ArrayList<Long> accountIds;
+
+    public GetAccountIdsResult(
+            int sessionStatus, int status, ArrayList<Long> accountIds) {
+
+        super(sessionStatus, status);
+
+        this.accountIds = accountIds;
+    }
+
+    public GetAccountIdsResult(int sessionStatus, int status, String message) {
+        super(sessionStatus, status, message);
+    }
+
+    public GetAccountIdsResult(int sessionStatus, String sessionMessage, int status) {
+        super(sessionStatus, sessionMessage, status);
+    }
+
+    public GetAccountIdsResult(int sessionStatus, String sessionMessage, int status, String message) {
+        super(sessionStatus, sessionMessage, status, message);
+    }
+
+    public ArrayList<Long> getAccountIds() {
+        return accountIds;
+    }
+
+
+}
