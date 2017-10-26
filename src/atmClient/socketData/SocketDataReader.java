f1793c2cd4f23ddf3bfc86b1fdfadb4660830392
chris3x33 <chris3x33g@gmail.com>
2017-10-26 10:32:06 -0500

    Created SocketDataReader

---
+++ src/atmClient/socketData/SocketDataReader.java
@@ -0,0 +1,12 @@
+package atmClient.socketData;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class SocketDataReader {
+
+
+
+}
