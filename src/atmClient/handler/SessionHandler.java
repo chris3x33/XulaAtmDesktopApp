a8767c75a2c8d81e64af1f5ad1ad90fbae3dfa2f
chris3x33 <chris3x33g@gmail.com>
2017-10-26 21:10:17 -0500

    Created +sendInvalidSession(dataOut:DataOutputStream):void in NewSessionResult

--- src/atmClient/handler/SessionHandler.java
+++ src/atmClient/handler/SessionHandler.java
@@ -1,7 +1,18 @@
 package atmClient.handler;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class SessionHandler {
 
     public static final String SOCKET_TIMEOUT_ERROR_MSG = "Unable to Connect Please Try again later!!";
     public static final String IO_EXCEPTION_ERROR_MSG = "Connection Error Please Try again later!!";
+    public static final int INVALID_SESSION = -1;
+
+    public static void sendInvalidSession(DataOutputStream dataOut) throws IOException {
+
+        dataOut.writeLong(INVALID_SESSION);
+        System.out.println("\tSent INVALID_SESSION id");
+
+    }
 }
