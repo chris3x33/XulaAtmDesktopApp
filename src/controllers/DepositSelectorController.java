bcdb6de25d6ddf46d0abb769d589607cb69c9723
chris3x33 <chris3x33g@gmail.com>
2017-11-03 15:07:26 -0500

    Created DepositSelectorScene

--- src/controllers/DepositSelectorController.java
+++ src/controllers/DepositSelectorController.java
@@ -3,10 +3,16 @@ package controllers;
 import atmClient.ATMClient;
 import atmClient.result.Result;
 import atmClient.result.SessionResult;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.Main;
 
 import java.io.IOException;
+
+import static atmClient.handler.SessionHandler.isValidSession;
+import static com.utils.Alerts.errorAlert;
 
 public class DepositSelectorController {
 
@@ -15,6 +21,8 @@ public class DepositSelectorController {
     public static final String APP_TITLE = Main.APP_TITLE;
     public static final Stage PRIMARY_STAGE = Main.primaryStage;
     public static ATMClient atmClient = Main.atmClient;
+
+    public static String DEPOSIT_SELECTOR_SCENE = Main.DEPOSIT_SELECTOR_SCENE;
 
     public void initialize() {
 
@@ -26,6 +34,52 @@ public class DepositSelectorController {
 
     }
 
+    public static void handleSceneShow() throws IOException{
 
+        //init DEPOSIT_SELECTOR_SCENE
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                AccountListController.class.getResource(DEPOSIT_SELECTOR_SCENE)
+        );
+        Parent root = fxmlLoader.load();
+
+        //Get AccountListController
+        DepositSelectorController accountListController =
+                (DepositSelectorController) fxmlLoader.getController();
+
+        //Setup accountListController Data
+        SessionResult sessionResult = accountListController.initData();
+
+        int sessionStatus = sessionResult.getSessionStatus();
+        if (!isValidSession(sessionResult)){
+
+            errorAlert(sessionResult.getSessionMessage(),APP_TITLE);
+
+            ATMStartController.handleSceneShow();
+
+            return;
+        }
+
+        if (sessionStatus <= SessionResult.ERROR_CODE){
+
+            errorAlert(sessionResult.getSessionMessage(),APP_TITLE);
+
+            ATMStartController.handleSceneShow();
+
+            return;
+        }
+
+        int status = sessionResult.getStatus();
+        if (status <= Result.ERROR_CODE){
+
+            errorAlert( sessionResult.getMessage(),APP_TITLE);
+
+            return;
+        }
+
+        //Show DEPOSIT_SELECTOR_SCENE
+        PRIMARY_STAGE.setScene(new Scene(root, WINDOWWIDTH, WINDOWHEIGHT));
+        PRIMARY_STAGE.show();
+
+    }
 
 }
