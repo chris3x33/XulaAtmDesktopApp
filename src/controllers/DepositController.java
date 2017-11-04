0a19b7e8d8ad84153f7f32d5874dea17c06690ed
chris3x33 <chris3x33g@gmail.com>
2017-11-03 20:14:50 -0500

    Added to DepositScene layout

--- src/controllers/DepositController.java
+++ src/controllers/DepositController.java
@@ -1,9 +1,15 @@
 package controllers;
 
 import atmClient.ATMClient;
+import atmClient.result.Result;
+import atmClient.result.SessionResult;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import main.Main;
+
+import java.io.IOException;
 
 public class DepositController {
 
@@ -11,14 +17,21 @@ public class DepositController {
     public static final int WINDOWWIDTH = Main.WINDOWWIDTH;
     public static final String APP_TITLE = Main.APP_TITLE;
     public static final Stage PRIMARY_STAGE = Main.primaryStage;
+    
     public static ATMClient atmClient = Main.atmClient;
-    public TextField balanceTxt;
+    
     public TextField depositAmountTxt;
-
+    public Label accountIdLbl;
+    public Label accountBalanceLbl;
 
     public void initialize() {
 
     }
 
+    public void deposit(ActionEvent actionEvent) {
 
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
 }
